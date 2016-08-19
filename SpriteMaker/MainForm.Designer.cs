46b2e20263ebcfb9a0d0bac200aa34dbb4cd7222
Tomik148 <tchalupa1997@gmail.com>
2016-08-19 11:58:01 +0200

    Form is doneish

--- SpriteMaker/MainForm.Designer.cs
+++ SpriteMaker/MainForm.Designer.cs
@@ -55,9 +55,15 @@
             this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
             this.groupBox5 = new System.Windows.Forms.GroupBox();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.listView1 = new System.Windows.Forms.ListView();
             this.multiSprite = new System.Windows.Forms.CheckBox();
             this.buttonLoadImage = new System.Windows.Forms.Button();
+            this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.listView2 = new System.Windows.Forms.ListView();
+            this.groupBox6 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxMain)).BeginInit();
             this.groupBox2.SuspendLayout();
             this.groupBox3.SuspendLayout();
@@ -65,20 +71,25 @@
             this.groupBox4.SuspendLayout();
             this.flowLayoutPanel1.SuspendLayout();
             this.groupBox5.SuspendLayout();
+            this.flowLayoutPanel2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.groupBox6.SuspendLayout();
             this.SuspendLayout();
             // 
             // pictureBoxMain
             // 
-            this.pictureBoxMain.Location = new System.Drawing.Point(12, 41);
+            this.pictureBoxMain.Location = new System.Drawing.Point(0, 0);
             this.pictureBoxMain.Name = "pictureBoxMain";
             this.pictureBoxMain.Size = new System.Drawing.Size(192, 317);
+            this.pictureBoxMain.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
             this.pictureBoxMain.TabIndex = 0;
             this.pictureBoxMain.TabStop = false;
             // 
             // openFileDialog1
             // 
             this.openFileDialog1.FileName = "openFileDialog1";
-            this.openFileDialog1.Filter = "Image File|*.png,*.jpg";
+            this.openFileDialog1.Filter = "Image File|*.png;*.jpg";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
             // 
             // selectPivot
             // 
@@ -98,7 +109,7 @@
             this.selectPivot.Location = new System.Drawing.Point(9, 19);
             this.selectPivot.Name = "selectPivot";
             this.selectPivot.Size = new System.Drawing.Size(185, 21);
-            this.selectPivot.TabIndex = 2;
+            this.selectPivot.TabIndex = 10;
             // 
             // groupBox2
             // 
@@ -110,10 +121,10 @@
             this.groupBox2.Controls.Add(this.textBoxR);
             this.groupBox2.Controls.Add(this.textBoxT);
             this.groupBox2.Controls.Add(this.textBoxL);
-            this.groupBox2.Location = new System.Drawing.Point(3, 149);
+            this.groupBox2.Location = new System.Drawing.Point(3, 185);
             this.groupBox2.Name = "groupBox2";
             this.groupBox2.Size = new System.Drawing.Size(200, 80);
-            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabIndex = 100;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Border";
             // 
@@ -158,36 +169,40 @@
             this.textBoxB.Location = new System.Drawing.Point(129, 48);
             this.textBoxB.Name = "textBoxB";
             this.textBoxB.Size = new System.Drawing.Size(65, 20);
-            this.textBoxB.TabIndex = 0;
+            this.textBoxB.TabIndex = 9;
+            this.textBoxB.Text = "0";
             // 
             // textBoxR
             // 
             this.textBoxR.Location = new System.Drawing.Point(29, 48);
             this.textBoxR.Name = "textBoxR";
             this.textBoxR.Size = new System.Drawing.Size(65, 20);
-            this.textBoxR.TabIndex = 0;
+            this.textBoxR.TabIndex = 7;
+            this.textBoxR.Text = "0";
             // 
             // textBoxT
             // 
             this.textBoxT.Location = new System.Drawing.Point(129, 22);
             this.textBoxT.Name = "textBoxT";
             this.textBoxT.Size = new System.Drawing.Size(65, 20);
-            this.textBoxT.TabIndex = 0;
+            this.textBoxT.TabIndex = 8;
+            this.textBoxT.Text = "0";
             // 
             // textBoxL
             // 
             this.textBoxL.Location = new System.Drawing.Point(29, 22);
             this.textBoxL.Name = "textBoxL";
             this.textBoxL.Size = new System.Drawing.Size(65, 20);
-            this.textBoxL.TabIndex = 0;
+            this.textBoxL.TabIndex = 6;
+            this.textBoxL.Text = "0";
             // 
             // groupBox3
             // 
             this.groupBox3.Controls.Add(this.selectPivot);
-            this.groupBox3.Location = new System.Drawing.Point(3, 235);
+            this.groupBox3.Location = new System.Drawing.Point(3, 271);
             this.groupBox3.Name = "groupBox3";
             this.groupBox3.Size = new System.Drawing.Size(200, 54);
-            this.groupBox3.TabIndex = 3;
+            this.groupBox3.TabIndex = 100;
             this.groupBox3.TabStop = false;
             this.groupBox3.Text = "Pivot";
             // 
@@ -201,10 +216,10 @@
             this.groupBox1.Controls.Add(this.textBoxW);
             this.groupBox1.Controls.Add(this.textBoxY);
             this.groupBox1.Controls.Add(this.textBoxX);
-            this.groupBox1.Location = new System.Drawing.Point(3, 63);
+            this.groupBox1.Location = new System.Drawing.Point(3, 99);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(200, 80);
-            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabIndex = 100;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Position";
             // 
@@ -249,36 +264,40 @@
             this.textBoxH.Location = new System.Drawing.Point(129, 48);
             this.textBoxH.Name = "textBoxH";
             this.textBoxH.Size = new System.Drawing.Size(65, 20);
-            this.textBoxH.TabIndex = 0;
+            this.textBoxH.TabIndex = 5;
+            this.textBoxH.Text = "0";
             // 
             // textBoxW
             // 
             this.textBoxW.Location = new System.Drawing.Point(29, 48);
             this.textBoxW.Name = "textBoxW";
             this.textBoxW.Size = new System.Drawing.Size(65, 20);
-            this.textBoxW.TabIndex = 0;
+            this.textBoxW.TabIndex = 4;
+            this.textBoxW.Text = "0";
             // 
             // textBoxY
             // 
             this.textBoxY.Location = new System.Drawing.Point(129, 22);
             this.textBoxY.Name = "textBoxY";
             this.textBoxY.Size = new System.Drawing.Size(65, 20);
-            this.textBoxY.TabIndex = 0;
+            this.textBoxY.TabIndex = 3;
+            this.textBoxY.Text = "0";
             // 
             // textBoxX
             // 
             this.textBoxX.Location = new System.Drawing.Point(29, 22);
             this.textBoxX.Name = "textBoxX";
             this.textBoxX.Size = new System.Drawing.Size(65, 20);
-            this.textBoxX.TabIndex = 0;
+            this.textBoxX.TabIndex = 2;
+            this.textBoxX.Text = "0";
             // 
             // groupBox4
             // 
             this.groupBox4.Controls.Add(this.textBoxName);
-            this.groupBox4.Location = new System.Drawing.Point(3, 3);
+            this.groupBox4.Location = new System.Drawing.Point(3, 39);
             this.groupBox4.Name = "groupBox4";
             this.groupBox4.Size = new System.Drawing.Size(200, 54);
-            this.groupBox4.TabIndex = 4;
+            this.groupBox4.TabIndex = 100;
             this.groupBox4.TabStop = false;
             this.groupBox4.Text = "Name";
             // 
@@ -287,27 +306,30 @@
             this.textBoxName.Location = new System.Drawing.Point(9, 19);
             this.textBoxName.Name = "textBoxName";
             this.textBoxName.Size = new System.Drawing.Size(185, 20);
-            this.textBoxName.TabIndex = 0;
+            this.textBoxName.TabIndex = 1;
             // 
             // flowLayoutPanel1
             // 
+            this.flowLayoutPanel1.Controls.Add(this.panel1);
             this.flowLayoutPanel1.Controls.Add(this.groupBox4);
             this.flowLayoutPanel1.Controls.Add(this.groupBox1);
             this.flowLayoutPanel1.Controls.Add(this.groupBox2);
             this.flowLayoutPanel1.Controls.Add(this.groupBox3);
             this.flowLayoutPanel1.Controls.Add(this.groupBox5);
-            this.flowLayoutPanel1.Location = new System.Drawing.Point(210, 12);
+            this.flowLayoutPanel1.Controls.Add(this.button1);
+            this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(3, 3);
             this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-            this.flowLayoutPanel1.Size = new System.Drawing.Size(207, 346);
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(209, 418);
             this.flowLayoutPanel1.TabIndex = 5;
             // 
             // groupBox5
             // 
             this.groupBox5.Controls.Add(this.textBox1);
-            this.groupBox5.Location = new System.Drawing.Point(3, 295);
+            this.groupBox5.Location = new System.Drawing.Point(3, 331);
             this.groupBox5.Name = "groupBox5";
             this.groupBox5.Size = new System.Drawing.Size(200, 51);
-            this.groupBox5.TabIndex = 4;
+            this.groupBox5.TabIndex = 100;
             this.groupBox5.TabStop = false;
             this.groupBox5.Text = "Pixels per unit";
             // 
@@ -316,16 +338,25 @@
             this.textBox1.Location = new System.Drawing.Point(9, 19);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(185, 20);
-            this.textBox1.TabIndex = 3;
+            this.textBox1.TabIndex = 11;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(3, 388);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(200, 23);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Make XML";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // listView1
             // 
             this.listView1.FullRowSelect = true;
-            this.listView1.Location = new System.Drawing.Point(423, 12);
+            this.listView1.Location = new System.Drawing.Point(0, 0);
             this.listView1.MultiSelect = false;
             this.listView1.Name = "listView1";
             this.listView1.ShowGroups = false;
-            this.listView1.Size = new System.Drawing.Size(147, 346);
+            this.listView1.Size = new System.Drawing.Size(147, 343);
             this.listView1.TabIndex = 6;
             this.listView1.UseCompatibleStateImageBehavior = false;
             this.listView1.View = System.Windows.Forms.View.List;
@@ -333,35 +364,89 @@
             // multiSprite
             // 
             this.multiSprite.AutoSize = true;
-            this.multiSprite.Location = new System.Drawing.Point(93, 15);
+            this.multiSprite.Location = new System.Drawing.Point(84, 7);
             this.multiSprite.Name = "multiSprite";
             this.multiSprite.Size = new System.Drawing.Size(97, 17);
-            this.multiSprite.TabIndex = 7;
+            this.multiSprite.TabIndex = 100;
+            this.multiSprite.TabStop = false;
             this.multiSprite.Text = "Multiple Sprites";
             this.multiSprite.UseVisualStyleBackColor = true;
             this.multiSprite.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
             // 
             // buttonLoadImage
             // 
-            this.buttonLoadImage.Location = new System.Drawing.Point(12, 12);
+            this.buttonLoadImage.Location = new System.Drawing.Point(3, 3);
             this.buttonLoadImage.Name = "buttonLoadImage";
             this.buttonLoadImage.Size = new System.Drawing.Size(75, 23);
-            this.buttonLoadImage.TabIndex = 8;
+            this.buttonLoadImage.TabIndex = 0;
             this.buttonLoadImage.Text = "Load Image";
             this.buttonLoadImage.UseVisualStyleBackColor = true;
+            this.buttonLoadImage.Click += new System.EventHandler(this.buttonLoadImage_Click);
+            // 
+            // flowLayoutPanel2
+            // 
+            this.flowLayoutPanel2.AutoSize = true;
+            this.flowLayoutPanel2.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.flowLayoutPanel2.Controls.Add(this.flowLayoutPanel1);
+            this.flowLayoutPanel2.Controls.Add(this.groupBox6);
+            this.flowLayoutPanel2.Location = new System.Drawing.Point(209, 12);
+            this.flowLayoutPanel2.Name = "flowLayoutPanel2";
+            this.flowLayoutPanel2.Size = new System.Drawing.Size(421, 424);
+            this.flowLayoutPanel2.TabIndex = 9;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.buttonLoadImage);
+            this.panel1.Controls.Add(this.multiSprite);
+            this.panel1.Location = new System.Drawing.Point(3, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(194, 30);
+            this.panel1.TabIndex = 10;
+            // 
+            // listView2
+            // 
+            this.listView2.Location = new System.Drawing.Point(6, 17);
+            this.listView2.MultiSelect = false;
+            this.listView2.Name = "listView2";
+            this.listView2.ShowGroups = false;
+            this.listView2.Size = new System.Drawing.Size(188, 365);
+            this.listView2.TabIndex = 10;
+            this.listView2.UseCompatibleStateImageBehavior = false;
+            this.listView2.View = System.Windows.Forms.View.List;
+            // 
+            // groupBox6
+            // 
+            this.groupBox6.Controls.Add(this.listView2);
+            this.groupBox6.Controls.Add(this.button2);
+            this.groupBox6.Location = new System.Drawing.Point(218, 3);
+            this.groupBox6.Name = "groupBox6";
+            this.groupBox6.Size = new System.Drawing.Size(200, 418);
+            this.groupBox6.TabIndex = 11;
+            this.groupBox6.TabStop = false;
+            this.groupBox6.Text = "Sprites";
+            this.groupBox6.Visible = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(6, 388);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(188, 23);
+            this.button2.TabIndex = 101;
+            this.button2.Text = "Make XML";
+            this.button2.UseVisualStyleBackColor = true;
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(580, 416);
-            this.Controls.Add(this.buttonLoadImage);
-            this.Controls.Add(this.multiSprite);
-            this.Controls.Add(this.listView1);
-            this.Controls.Add(this.flowLayoutPanel1);
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(1093, 554);
+            this.Controls.Add(this.flowLayoutPanel2);
             this.Controls.Add(this.pictureBoxMain);
             this.Name = "MainForm";
-            this.Text = "Form1";
+            this.Text = "Sprite Editor";
+            this.Load += new System.EventHandler(this.MainForm_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxMain)).EndInit();
             this.groupBox2.ResumeLayout(false);
             this.groupBox2.PerformLayout();
@@ -373,6 +458,10 @@
             this.flowLayoutPanel1.ResumeLayout(false);
             this.groupBox5.ResumeLayout(false);
             this.groupBox5.PerformLayout();
+            this.flowLayoutPanel2.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.groupBox6.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -410,6 +499,12 @@
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.CheckBox multiSprite;
         private System.Windows.Forms.Button buttonLoadImage;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.GroupBox groupBox6;
+        private System.Windows.Forms.ListView listView2;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- SpriteMaker/MainForm.cs
+++ SpriteMaker/MainForm.cs
@@ -19,6 +19,37 @@ namespace SpriteMaker
 
         private void checkBox1_CheckedChanged(object sender, EventArgs e)
         {
+            if(groupBox6.Visible)
+            {
+                groupBox6.Visible = false;
+                button1.Text = "Make XML";
+            }
+            else
+            {
+                groupBox6.Visible = true;
+                button1.Text = "Add Sprite";
+            }
+        }
+
+        private void buttonLoadImage_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            //MessageBox.Show(openFileDialog1.FileName);
+            //TODO: Check for XML file and load it
+            pictureBoxMain.Image = new Bitmap(openFileDialog1.FileName);
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
 
         }
     }
